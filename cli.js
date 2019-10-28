7302d3421cf256d2feb3a597ce7436602354e85e
Brian Zelip <bzelip@gmail.com>
2019-10-28 15:15:49 -0400

    cli.js: Update code while working through yargs

--- cli.js
+++ cli.js
@@ -1,4 +1,6 @@
-// const yargs = require('yargs');
+#!/usr/bin/env node
+
+const yargs = require('yargs');
 
 // const message = yargs => {
 //   yargs.positional('message', {
@@ -12,12 +14,12 @@
 // const argv = yargs(['--info'])
 //   .scriptName('bump')
 //   .help('info')
+//   .usage()
 //   .command(
 //     ['major <message>', 'M <message>'],
 //     "Bump the version's major number, set the minor and patch numbers to zero, and append message to the generated commit message.",
 //     yargs => {
 //       yargs.positional('message', {
-//         alias: ['m', 'msg'],
 //         describe:
 //           'a string that describes the reason for the version bump, to be appended to the generated commit message',
 //         type: 'string'
@@ -29,7 +31,6 @@
 //     "Bump the version's minor number, set the patch number to zero, and append message to the generated commit message.",
 //     yargs => {
 //       yargs.positional('message', {
-//         alias: ['m', 'msg'],
 //         describe:
 //           'a string that describes the reason for the version bump, to be appended to the generated commit message',
 //         type: 'string'
@@ -41,7 +42,6 @@
 //     "Bump the version's patch number, and append message to the generated commit message.",
 //     yargs => {
 //       yargs.positional('message', {
-//         alias: ['m', 'msg'],
 //         describe:
 //           'a string that describes the reason for the version bump, to be appended to the generated commit message',
 //         type: 'string'
@@ -51,16 +51,19 @@
 
 // console.log(argv);
 
-const argv = require('yargs')('run --help').command(
-  'run <port> <guid>',
-  'run the server',
-  yargs => {
-    yargs.positional('message', {
-      alias: ['m', 'msg'],
-      describe:
-        'a string that describes the reason for the version bump, to be appended to the generated commit message',
-      type: 'string'
-    });
-  }
-).argv;
-console.log(argv);
+// const argv = require('yargs')('run --help').command(
+//   'run <port> <guid>',
+//   'run the server',
+//   yargs => {
+//     yargs.positional('message', {
+//       describe:
+//         'a string that describes the reason for the version bump, to be appended to the generated commit message',
+//       type: 'string'
+//     });
+//   }
+// ).argv;
+// console.log(argv);
+
+const argv = yargs.scriptName('bump');
+
+console.log(argv.argv);
