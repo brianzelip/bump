ba41bbfec5c70d7e7d1e85f9aaf17e738b149945
Brian Zelip <bzelip@gmail.com>
2019-10-29 07:33:01 -0400

    examples.js: Rename cli.js

--- cli.js
+++ cli.js
@@ -1,69 +1,44 @@
-#!/usr/bin/env node
-
 const yargs = require('yargs');
+const chalk = require('chalk');
 
-// const message = yargs => {
-//   yargs.positional('message', {
-//     alias: ['m', 'msg'],
-//     describe:
-//       'a string that describes the reason for the version bump, to be appended to the generated commit message',
-//     type: 'string'
-//   });
-// };
+const argv = yargs
+  .scriptName('bump')
+  .usage(
+    chalk`
+{rgb(237, 112, 50) @bzelip/bump 🍑
 
-// const argv = yargs(['--info'])
-//   .scriptName('bump')
-//   .help('info')
-//   .usage()
-//   .command(
-//     ['major <message>', 'M <message>'],
-//     "Bump the version's major number, set the minor and patch numbers to zero, and append message to the generated commit message.",
-//     yargs => {
-//       yargs.positional('message', {
-//         describe:
-//           'a string that describes the reason for the version bump, to be appended to the generated commit message',
-//         type: 'string'
-//       });
-//     }
-//   )
-//   .command(
-//     ['minor <message>', 'N <message>'],
-//     "Bump the version's minor number, set the patch number to zero, and append message to the generated commit message.",
-//     yargs => {
-//       yargs.positional('message', {
-//         describe:
-//           'a string that describes the reason for the version bump, to be appended to the generated commit message',
-//         type: 'string'
-//       });
-//     }
-//   )
-//   .command(
-//     ['patch <message>', 'P <message>'],
-//     "Bump the version's patch number, and append message to the generated commit message.",
-//     yargs => {
-//       yargs.positional('message', {
-//         describe:
-//           'a string that describes the reason for the version bump, to be appended to the generated commit message',
-//         type: 'string'
-//       });
-//     }
-//   ).argv;
+Bump your package.json and package-lock.json files, and commit the change with a meaningful description.}
+`
+  )
+  .command(
+    '$0 <strategy> <message>',
+    'Bump your version based on a strategy, and include a description of the reason for the bump in the generated commit message.',
+    yargs => {
+      yargs
+        .positional('strategy', {
+          describe: 'strategy for the version bump',
+          type: 'string',
+          choices: ['major', 'minor', 'patch']
+        })
+        .positional('message', {
+          describe:
+            'description of the reason for the version bump, to be appended to the generated commit message',
+          type: 'string'
+        });
+    },
+    argv => {
+      console.log('this command will be run by default');
+    }
+  )
+  .command(
+    ['undo', 'u'],
+    'undo the last bump',
+    yargs => {},
+    argv => {
+      console.log('this is the undo command');
+    }
+  )
+  .alias('help', 'h')
+  .alias('version', 'v').argv;
 
-// console.log(argv);
-
-// const argv = require('yargs')('run --help').command(
-//   'run <port> <guid>',
-//   'run the server',
-//   yargs => {
-//     yargs.positional('message', {
-//       describe:
-//         'a string that describes the reason for the version bump, to be appended to the generated commit message',
-//       type: 'string'
-//     });
-//   }
-// ).argv;
-// console.log(argv);
-
-const argv = yargs.scriptName('bump');
-
-console.log(argv.argv);
+console.log(argv);
