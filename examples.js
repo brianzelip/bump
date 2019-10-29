5daf993ec0453ba1e626c910fccfbe9b30ea475d
Brian Zelip <bzelip@gmail.com>
2019-10-29 06:13:25 -0400

    examples.js: Clean up debuggers

--- examples.js
+++ examples.js
@@ -1,57 +1,5 @@
 const yargs = require('yargs');
 
-// const argv = yargs
-//   .scriptName('bump')
-//   .command(
-//     ['major <message>', 'M <message>'],
-//     "Bump the version's major number, set the minor and patch numbers to zero, and append message to the generated commit message.",
-//     function(yargs) {
-//       return yargs.option('url', {
-//         alias: 'u',
-//         default: 'http://yargs.js.org/'
-//       });
-//     }
-//   )
-//   .command(
-//     ['minor <message>', 'N <message>'],
-//     "Bump the version's minor number, set the patch numbers to zero, and append message to the generated commit message.",
-//     yargs => {
-//       yargs.positional('message', {
-//         describe: 'message to append to the generated commit message',
-//         type: 'string'
-//       });
-//     }
-//   )
-//   .command(
-//     ['patch <message>', 'P <message>'],
-//     "Bump the version's patch number and append message to the generated commit message.",
-//     yargs => {
-//       yargs.positional('message', {
-//         describe: 'message to append to the generated commit message',
-//         type: 'string'
-//       });
-//     }
-//   )
-//   .wrap(yargs.terminalWidth()).argv;
-
-// const argv = yargs
-//   .scriptName('bump')
-//   .command(
-//     '$0 -s -m',
-//     'the default command',
-//     yargs => {
-//       yargs.option('s', {
-//         alias: 'strategy',
-//         describe: 'strategy for the version bump',
-//         choices: ['major', 'minor', 'patch']
-//       });
-//     },
-//     argv => {
-//       console.log('this command will be run by default');
-//     }
-//   )
-//   .wrap(yargs.terminalWidth()).argv;
-
 const argv = yargs.scriptName('bump').usage(
   '$0 <strategy> <message>',
   'Bump your version given a strategy, and add content to a git commit message describing the bump.',
