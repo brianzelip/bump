c4c16c2b0ee8328873f7791870603566aa5d1835
Brian Zelip <bzelip@gmail.com>
2019-10-29 17:05:41 -0400

    src/: Refactor strategy module into 2 files, move all module files into src/modules

--- src/modules/strategy-handler.js
+++ src/modules/strategy-handler.js
@@ -4,20 +4,6 @@ const path = require('path');
 const { EOL } = require('os');
 const c_p = require('child_process');
 
-exports.builder = yargs => {
-  yargs
-    .positional('strategy', {
-      describe: 'strategy for the version bump',
-      type: 'string',
-      choices: ['major', 'minor', 'patch']
-    })
-    .positional('message', {
-      describe:
-        'description of the reason for the version bump, to be appended to the generated commit message',
-      type: 'string'
-    });
-};
-
 exports.handler = argv => {
   // set up data
   const cwd = process.cwd();
