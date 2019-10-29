22b4720b454bf698c0006c4015e538a25bf5677b
Brian Zelip <bzelip@gmail.com>
2019-10-29 07:44:48 -0400

    cli.js: Replace builders and handlers with command modules

--- cli.js
+++ cli.js
@@ -13,31 +13,9 @@ Bump your package.json and package-lock.json files, and commit the change with a
   .command(
     '$0 <strategy> <message>',
     'Bump your version based on a strategy, and include a description of the reason for the bump in the generated commit message.',
-    yargs => {
-      yargs
-        .positional('strategy', {
-          describe: 'strategy for the version bump',
-          type: 'string',
-          choices: ['major', 'minor', 'patch']
-        })
-        .positional('message', {
-          describe:
-            'description of the reason for the version bump, to be appended to the generated commit message',
-          type: 'string'
-        });
-    },
-    argv => {
-      console.log('this command will be run by default');
-    }
+    require('./strategy.js')
   )
-  .command(
-    ['undo', 'u'],
-    'undo the last bump',
-    yargs => {},
-    argv => {
-      console.log('this is the undo command');
-    }
-  )
+  .command(['undo', 'u'], 'undo the last bump', require('./undo.js'))
   .alias('help', 'h')
   .alias('version', 'v').argv;
 
