2ab987dd273a0a52183f5936188b9709386abbeb
Brian Zelip <bzelip@gmail.com>
2019-10-29 22:00:40 -0400

    cli.js: Rename bump.js

--- src/bump.js
+++ src/bump.js
@@ -1,106 +1,22 @@
-#!/usr/bin/env node
-
-const fs = require('fs');
-const path = require('path');
-const { EOL } = require('os');
-const c_p = require('child_process');
-
+const yargs = require('yargs');
 const chalk = require('chalk');
 
-const strategy = process.argv[2];
-const message = process.argv[3];
-const cwd = process.cwd();
-const pathToPackage = path.join(cwd, 'package.json');
-const pathToPackageLock = path.join(cwd, 'package-lock.json');
+const argv = yargs
+  .scriptName('bump')
+  .usage(
+    chalk`
+{rgb(237, 112, 50) @bzelip/bump 🍑
 
-const oldPackageJson = JSON.parse(fs.readFileSync(pathToPackage));
-const oldPackageLockJson = JSON.parse(fs.readFileSync(pathToPackageLock));
+Bump your package.json and package-lock.json files, and commit the change with a meaningful description.}
+`
+  )
+  .command(
+    '$0 <strategy> <message>',
+    'Bump your version based on a strategy, and include a description of the reason for the bump in the generated commit message.',
+    require('./modules/strategy.js')
+  )
+  .command(['undo', 'u'], 'undo the last bump', require('./modules/undo.js'))
+  .alias('help', 'h')
+  .alias('version', 'v').argv;
 
-const oldVersionAsString = JSON.parse(fs.readFileSync(pathToPackage)).version;
-
-const oldVersionAsArray = oldVersionAsString
-  .split('.')
-  .map(number => Number(number));
-
-function isMajor() {
-  return strategy === 'm' || strategy === 'major';
-}
-
-function strategyArray() {
-  if (strategy === 'm' || strategy === 'major') {
-    return ['increase', 'reset', 'reset'];
-  }
-  if (strategy === 'n' || strategy === 'minor') {
-    return ['same', 'increase', 'reset'];
-  }
-  if (strategy === 'p' || strategy === 'patch') {
-    return ['same', 'same', 'increase'];
-  }
-  return undefined;
-}
-
-function transform(action, num) {
-  return action === 'reset'
-    ? 0
-    : action === 'increase'
-    ? num + 1
-    : action === 'same'
-    ? num
-    : undefined;
-}
-
-const newVersionAsArray = strategyArray().map((action, index) => {
-  return transform(action, oldVersionAsArray[index]);
-});
-
-const newVersionAsString = newVersionAsArray.join('.');
-
-const filesChanged = 'package*';
-const woohoo = '🎉';
-const shipped = '🚢';
-
-const rcCommit = `${filesChanged}: v${newVersionAsString} Bump ${strategy} for ${message} ${
-  isMajor() ? `${shipped}  ${woohoo}` : woohoo
-}`;
-
-const newPackageJson = oldPackageJson;
-const newPackageLockJson = oldPackageLockJson;
-
-newPackageJson.version = newVersionAsString;
-newPackageLockJson.version = newVersionAsString;
-
-function updatePackageFiles() {
-  fs.writeFileSync(
-    pathToPackage,
-    `${JSON.stringify(newPackageJson, null, 2)}${EOL}`
-  );
-  fs.writeFileSync(
-    pathToPackageLock,
-    `${JSON.stringify(newPackageLockJson, null, 2)}${EOL}`
-  );
-}
-
-function addPackages() {
-  c_p.execSync(`git add package*`, { encoding: 'utf8' });
-}
-
-function commitBump() {
-  c_p.execSync(`git commit -m "${rcCommit}"`, { encoding: 'utf8' });
-}
-
-function confirm() {
-  console.log(chalk`
-{rgb(237, 112, 50) 🍑 : Successfully {underline ${strategy}} bumped version to {underline ${newVersionAsString}} with the commit message:
-
-"${rcCommit} "
-}`);
-}
-
-function bump() {
-  updatePackageFiles();
-  addPackages();
-  commitBump();
-  confirm();
-}
-
-bump();
+console.log(argv);
