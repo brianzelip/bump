faf8f8f3888e367aba654f50ec485c2c48ce2b80
Brian Zelip <bzelip@gmail.com>
2019-10-29 22:16:51 -0400

    undo.js: Create undo functionality

--- src/modules/undo.js
+++ src/modules/undo.js
@@ -1,5 +1,31 @@
+const path = require('path');
+const c_p = require('child_process');
+const chalk = require('chalk');
+
 exports.builder = yargs => {};
 
+// handler assumes being called immediately after bump <strategy> <message> was called
 exports.handler = argv => {
+  function gitResetHead() {
+    c_p.execSync(`git reset HEAD~`, { encoding: 'utf8' });
+  }
+
+  function gitCheckoutPackages() {
+    c_p.execSync(`git checkout -- package*`, { encoding: 'utf8' });
+  }
+
+  function confirm() {
+    console.log(chalk`
+{rgb(237, 112, 50) 🍑 : Successfully undo the prior bump and commit}`);
+  }
+
+  function write() {
+    gitResetHead();
+    gitCheckoutPackages();
+    confirm();
+  }
+
+  write();
+
   console.log('UNDO HANDLER fn', argv);
 };
